45299392fe6578cf1961379d0de28686fa74f766
Asia Antczak <aasiaantczak@gmail.com>
2018-05-29 18:08:57 +0100

    add compare array test, and use it

--- spec/assert.js
+++ spec/assert.js
@@ -5,5 +5,25 @@ var assert = {
     } else {
       console.log(`%c${testDescription} passed: ${testCondition}`, 'color: green');
     }
+  },
+
+  isEqual: function(testArg1, testArg2, testDescription) {
+    if (testArg1 != testArg2) {
+      throw new Error(`${testDescription} failed: ${testArg1} is not equal ${testArg2}`);
+    } else {
+      console.log(`%c${testDescription} passed: ${testArg1} is equal ${testArg2}`, 'color: green');
+    }
+  },
+
+  arraysAreEqual: function(testArray1, testArray2, testDescription) {
+    if (testArray1.length != testArray2.length) {
+      throw new Error(`${testDescription} failed: ${testArray1} is not equal ${testArray2}`);
+    }
+    for (var i = 0; i < testArray1.length; i++) {
+      if (testArray1[i] != testArray2[i]) {
+        throw new Error(`${testDescription} failed: ${testArray1} is not equal ${testArray2}`);
+      }
+    }
+    console.log(`%c${testDescription} passed: ${testArray1} is equal ${testArray2}`, 'color: green');
   }
 };
--- spec/note-list-model-spec.js
+++ spec/note-list-model-spec.js
@@ -1,10 +1,22 @@
 (function(exports) {
   function testStoresArrayOfNotes() {
     var noteList = new NoteList();
-    assert.isTrue(noteList.notes, 'storesAnEmptyArray');
+    assert.isEqual(noteList.notes.length, 0, 'storesAnEmptyArray');
     noteList.notes.push(new Note('note 1'));
     noteList.notes.push(new Note('note 2'));
-    assert.isTrue(noteList.notes.length === 2, 'storesArrayOfNotes');
+    assert.isEqual(noteList.notes.length, 2, 'storesArrayOfNotes');
   }
   testStoresArrayOfNotes();
 })(this);
+
+(function(exports) {
+  function testReturnAllTheNotesStored() {
+    var noteList = new NoteList();
+    var note1 = new Note('note 1');
+    var note2 = new Note('note 2');
+    noteList.notes.push(note1);
+    noteList.notes.push(note2);
+    assert.arraysAreEqual(noteList.notes, [note1, note2], 'testReturnAllTheNotesStored')
+  }
+  testReturnAllTheNotesStored();
+})(this)
