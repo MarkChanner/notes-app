03f6588e8f9dfa5991626c10afaeaea951424b3f
Mark Channer <markchanner@gmail.com>
2018-05-30 12:51:14 +0100

    Add check condition to getHTML method for when noteList contains no notes

--- spec/note-list-view-spec.js
+++ spec/note-list-view-spec.js
@@ -1,13 +1,26 @@
 (function(exports) {
-  function testReturnNoteHTML(){
+  function testReturnNoteHTML() {
     var noteList = new NoteList();
     var note1 = new Note('note 1');
     var note2 = new Note('note 2');
     noteList.notes.push(note1);
     noteList.notes.push(note2);
     var noteListView = new NoteListView(noteList);
-    var html = '<ul><li><div>note 1</div></li><li><div>note 2</div></li></ul>'
-    assert.isEqual(noteListView.getHTML(), html,'testReturnNoteHTML')
+    var html = '<ul><li><div>note 1</div></li><li><div>note 2</div></li></ul>';
+    assert.isEqual(noteListView.getHTML(), html, 'testReturnNoteHTML');
   }
+
+  function testReturnEmptyString() {
+    var emptyNoteList = new NoteList();
+    var noteListView = new NoteListView(emptyNoteList);
+    var emptyString = '';
+    assert.isEqual(
+      noteListView.getHTML(),
+      emptyString,
+      'testReturnEmptyString'
+    );
+  }
+
   testReturnNoteHTML();
-})(this)
+  testReturnEmptyString();
+})(this);
--- src/note-list-view.js
+++ src/note-list-view.js
@@ -1,16 +1,15 @@
 (function(exports) {
-
   function NoteListView(noteList) {
     this.noteList = noteList;
   }
 
-  NoteListView.prototype.getHTML = function () {
+  NoteListView.prototype.getHTML = function() {
     var notes = this.noteList.getNotes();
-    var notesHTML = []
+    var notesHTML = [];
     notes.forEach(function(note) {
       notesHTML.push('<li><div>' + note.text + '</div></li>');
     });
-    return '<ul>' + notesHTML.join("") + '</ul>';
+    return notesHTML.length != 0 ? '<ul>' + notesHTML.join('') + '</ul>' : '';
   };
 
   exports.NoteListView = NoteListView;
