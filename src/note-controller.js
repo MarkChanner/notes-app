8abeaca5ffa5155a4a2bef65e7024ab11747bb8f
Asia Antczak <aasiaantczak@gmail.com>
2018-05-30 15:57:24 +0100

    add controller

--- src/note-controller.js
+++ src/note-controller.js
@@ -1,3 +1,19 @@
 window.onload = function() {
-  document.querySelector('#app').textContent = 'Howdy';
+  (function (exports) {
+    function NoteController(noteList) {
+      this.noteList = noteList
+      this.noteList.createNote("Favourite drink: seltzer");
+      this.noteView = new NoteListView(this.noteList);
+    }
+
+    NoteController.prototype.updateView = function() {
+      var htmlText = this.noteView.getHTML()
+      document.querySelector('#app').innerHTML = htmlText;
+    };
+    
+    exports.NoteController = NoteController;
+  })(this);
+
+  var noteController = new NoteController(new NoteList());
+  noteController.updateView();
 };
