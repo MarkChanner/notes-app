90ae167890a760be7bd422120a8507dd1f06ccd8
Mark Channer <markchanner@gmail.com>
2018-06-01 15:28:41 +0100

    Refactors testReturnNoteHTML test

--- spec/note-list-view-spec.js
+++ spec/note-list-view-spec.js
@@ -1,10 +1,8 @@
 (function() {
   function testReturnNoteHTML() {
     var noteList = new NoteList();
-    var note1 = new Note('note 1 over 20 characters');
-    var note2 = new Note('note 2 over 20 characters');
-    noteList.notes.push(note1);
-    noteList.notes.push(note2);
+    noteList.createNote('note 1 over 20 characters');
+    noteList.createNote('note 2 over 20 characters');
     var noteListView = new NoteListView(noteList);
     var html =
       '<ul><li><div>note 1 over 20 chara</div></li><li><div>note 2 over 20 chara</div></li></ul>';
