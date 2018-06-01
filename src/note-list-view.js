0658abd39353200a92b86556b9699df243d72e87
Mark Channer <markchanner@gmail.com>
2018-06-01 14:39:10 +0100

    Refactors getHTML method

--- src/note-list-view.js
+++ src/note-list-view.js
@@ -4,9 +4,8 @@
   }
 
   NoteListView.prototype.getHTML = function() {
-    var notes = this.noteList.getNotes();
     var notesHTML = [];
-    notes.forEach(function(note) {
+    this.noteList.getNotes().forEach(function(note) {
       notesHTML.push('<li><div>' + note.text.substring(0, 20) + '</div></li>');
     });
     return notesHTML.length != 0 ? '<ul>' + notesHTML.join('') + '</ul>' : '';
