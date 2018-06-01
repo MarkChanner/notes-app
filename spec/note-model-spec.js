45c04046e5d6ab6eb6229fdb17eba38c3c6513ff
Mark Channer <markchanner@gmail.com>
2018-06-01 15:29:52 +0100

    Removes redundant test

--- spec/note-model-spec.js
+++ spec/note-model-spec.js
@@ -1,18 +1,9 @@
-// Has a method that will return the note text
-(function() {
-  function testNoteStoresTextWhenInstantiated() {
-    var testText = 'My favourite language is JavaScript';
-    var note = new Note(testText);
-    assert.isTrue(note.text === testText, 'testNoteStoresTextWhenInstantiated');
-  }
-  testNoteStoresTextWhenInstantiated();
-})(this);
-
 (function() {
   function testGetTextMethodReturnsText() {
     var testText = 'My favourite language is JavaScript';
     var note = new Note(testText);
-    assert.isTrue(note.getText() === testText, 'testGetTextMethodReturnsText');
+    assert.isEqual(note.getText(), testText, 'testGetTextMethodReturnsText');
   }
+
   testGetTextMethodReturnsText();
 })(this);
