c1f4174268c89e4e15aac2c7e3c16decf7087794
Mark Channer <markchanner@gmail.com>
2018-06-01 15:22:25 +0100

    Removes redundant test

--- spec/note-list-model-spec.js
+++ spec/note-list-model-spec.js
@@ -1,24 +1,11 @@
 (function() {
-  function testCreateAndStoreNewNote() {
-    var noteList = new NoteList();
-    var text = 'Favorite drink: seltzer';
-    noteList.createNote(text);
-    assert.isEqual(
-      noteList.notes.pop().text,
-      text,
-      'testCreateAndStoreNewNote'
-    );
-  }
-  testCreateAndStoreNewNote();
-})(this);
-
-(function() {
-  function testReturnAllTheNotesStored() {
+  function testCreatesAndReturnsStoredNotes() {
     var noteList = new NoteList();
     noteList.createNote('note 1');
     noteList.createNote('note 2');
     assert.isEqual(noteList.getNotes()[0].getText(), 'note 1');
     assert.isEqual(noteList.getNotes()[1].getText(), 'note 2');
   }
-  testReturnAllTheNotesStored();
+
+  testCreatesAndReturnsStoredNotes();
 })(this);
